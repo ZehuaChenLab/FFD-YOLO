e9aa8dfac49e53b6c7afb42bae995009142b2911
ZehuaChenLab <115785526+ZehuaChenLab@users.noreply.github.com>
2025-02-17 11:08:51 +0800

    Add files via upload

---
+++ LEIEM.py
@@ -0,0 +1,68 @@
+from ultralytics.nn.modules.block import C2f, Conv
+import torch.nn as nn
+import torch
+import numpy as np
+
+
+class GhostConv(nn.Module):
+    """Ghost Convolution https://github.com/huawei-noah/ghostnet."""
+
+    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+        """Initializes Ghost Convolution module with primary and cheap operations for efficient feature learning."""
+        super().__init__()
+        c_ = c2 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act=act)
+
+    def forward(self, x):
+        """Forward propagation through a Ghost Bottleneck layer with skip connection."""
+        y = self.cv1(x)
+        return torch.cat((y, self.cv2(y)), 1)
+
+
+class SobelConv(nn.Module):
+    def __init__(self, channel) -> None:
+        super().__init__()
+
+        sobel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+        sobel_kernel_y = torch.tensor(sobel, dtype=torch.float32).unsqueeze(0).expand(channel, 1, 1, 3, 3)
+        sobel_kernel_x = torch.tensor(sobel.T, dtype=torch.float32).unsqueeze(0).expand(channel, 1, 1, 3, 3)
+
+        self.sobel_kernel_x_conv3d = nn.Conv3d(channel, channel, kernel_size=3, padding=1, groups=channel, bias=False)
+        self.sobel_kernel_y_conv3d = nn.Conv3d(channel, channel, kernel_size=3, padding=1, groups=channel, bias=False)
+
+        self.sobel_kernel_x_conv3d.weight.data = sobel_kernel_x.clone()
+        self.sobel_kernel_y_conv3d.weight.data = sobel_kernel_y.clone()
+
+        self.sobel_kernel_x_conv3d.requires_grad = False
+        self.sobel_kernel_y_conv3d.requires_grad = False
+
+    def forward(self, x):
+        return (self.sobel_kernel_x_conv3d(x[:, :, None, :, :]) + self.sobel_kernel_y_conv3d(x[:, :, None, :, :]))[:, :,
+               0]
+
+
+class LEIEM(nn.Module):
+    def __init__(self, inc, ouc) -> None:
+        super().__init__()
+
+        self.ghost = GhostConv(inc, inc)
+        self.sobel_branch = SobelConv(inc)
+        self.conv_branch = Conv(inc, inc, 3)
+        self.conv1 = Conv(inc * 2, inc, 1)
+        self.conv2 = Conv(inc, ouc, 1)
+
+    def forward(self, x):
+        x_ghost = self.ghost(x)
+        x_sobel = self.sobel_branch(x_ghost)
+        x_conv = self.conv_branch(x_ghost)
+        x_concat = torch.cat([x_sobel, x_conv], dim=1)
+        x_feature = self.conv1(x_concat)
+        x = x_feature + x
+        return x
+
+
+class C2f_LEIEM(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(LEIEM(self.c, self.c) for _ in range(n))
